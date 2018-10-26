cc5a60d54f6cef0f9ed0a3f7a63651e09295f1f6
James Radley <james.radley@ada.ac.uk>
2018-10-26 21:09:14 +0100

    Update manage.php

--- manage.php
+++ manage.php
@@ -46,7 +46,7 @@ $vehrow = mysql_fetch_array($vehquery);
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MPRPC | PNC</title>
+  <title>PNC</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
